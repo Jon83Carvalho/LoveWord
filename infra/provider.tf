76175d36f76b1f86afb3dd1c915f09246a44fbca
Jon83Carvalho <jonas.carvalho@gmail.com>
2021-10-03 20:17:19 -0300

    provider upt

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
     region = var.aws_region
 }
+
+
+#Centralizar arquivo de controle
+
+terraform {
+  backend "s3" {
+    bucket="terraformtfstatejcprj"
+    key="state/terraform.tfstate"
+    region="us-east-2"
+  }
+}

424b2b493414389f3eefc152ce595463cc2f5529
Jonas83Carvalho <jonas.carvalho@gmail.com>
2021-10-17 16:38:46 -0300

    teste

--- infra/s3.tf
+++ infra/s3.tf
@@ -16,6 +16,23 @@ resource "aws_s3_bucket" "dl" {
   }
 }
 
+resource "aws_s3_bucket" "dl" {
+  bucket = "dados-proj-jc-edc"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC-PROJETO"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
 
 # resource "aws_s3_bucket" "stream" {
 #   bucket = "igti-jonas-streaming-bucket"
